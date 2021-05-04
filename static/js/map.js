28219c67c06abb051018da83d37789f492912b1a
Rayna Mecham <rayna.mecham@gmail.com>
2021-05-03 22:42:32 -0600

    working on layout

--- static/js/map.js
+++ static/js/map.js
@@ -4,13 +4,15 @@
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    mapId: "7c96088359f57898",
+    mapId: "40d6a623f18f9a8e",
     center: { lat: 51.507351, lng: -0.127758 },
     zoom: 5,
     scrollwheel: true,
     zoomControl: true,
     panControl: false,
-    streetViewControl: false
+    streetViewControl: false,
+    fullscreenControl: false,
+    mapTypeControl: false
   });
 
   const locationInfo = new google.maps.InfoWindow();
