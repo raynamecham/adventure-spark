2155314ae74a01c38fa9aa31bf1a9a89e8d823a1
Rayna Mecham <rayna.mecham@gmail.com>
2021-06-07 11:14:47 -0600

    adding readme file

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,27 +1,6 @@
-import time
 from selenium import webdriver
 
-# browser = webdriver.Chrome('path/to/chromedriver')
-# browser.get('http://localhost:5000')
-# assert browser.title == 'Adventure Spark'
-
-# email = browser.find_element_by_id('login-email')
-# email.send_keys("admin@gmail.com")
-# password = browser.find_element_by_id('login-password')
-# password.send_keys("admin")
-
-# btn = browser.find_element_by_id('login-button')
-# btn.click()
-
-# result = browser.find_element_by_id('result')
-# assert result.text == "7"
-# browser.close()
-
-driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com/');
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+# Set test variables for test admin user
+test_admin_username = "admin"
+test_admin_email = "admin@email.com"
+test_admin_password = "admin2016"
